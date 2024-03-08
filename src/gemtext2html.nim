8c1334d268361d1cd20c8efce2cde7d8ad1f4c63
Azumabashi <>
2024-03-09 00:21:41 +0900

    add proc convertFromFile

--- src/gemtext2html.nim
+++ src/gemtext2html.nim
@@ -105,6 +105,9 @@ proc convert*(gemtext: string): string =
   # wrap with `html` and `body`
   return html(body(content.join("\n")))
 
+proc convertFromFile*(path: string): string =
+  path.readFile.convert
+
 when isMainModule:
   const text = """
 # This is text
