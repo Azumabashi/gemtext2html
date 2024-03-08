14464f0133d7e7be2f198b09097e0e1c654a1704
Azumabashi <>
2024-03-09 00:56:43 +0900

    add test

---
+++ tests/test1.nim
@@ -0,0 +1,65 @@
+import unittest
+import strutils
+
+import gemtext2html
+
+test "check conversion is executed correctly":
+  const gemtext = """
+# This is text
+## subtext
+Hello!
+=> https://example.com
+=> https://example.com example
+
+another
+text
+
+new paragraph
+
+## another subtext
+### subsubtext
+aaa
+## yet another subtext
+bbb
+```foo
+f x y = x + y
+```
+```
+g x = x * x
+```
+
+End!
+"""
+  const expected = """
+<html>
+<body>
+<h1>This is text</h1>
+<h2>subtext</h2>
+<p>Hello!</p>
+<p>
+<a href="https://example.com">https://example.com</a><br>
+<a href="https://example.com">example</a>
+</p>
+<p>
+another<br>text
+</p>
+<p>new paragraph</p>
+
+<h2>another subtext</h2>
+<h3>subsubtext</h3>
+<p>aaa</p>
+
+<h2>yet another subtext</h2>
+<p>bbb</p>
+<figure>
+<pre>f x y = x + y</pre>
+<figcaption>foo</figcaption>
+</figure>
+<figure>
+<pre>g x = x * x</pre>
+</figure>
+<p>End!</p>
+</body>
+</html>
+"""
+  check gemtext.convert.replace("\n", "") == expected.replace("\n", "")
